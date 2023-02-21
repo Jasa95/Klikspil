15cebeeb1cb113469dfcdf9c65d29a0dd3d79b96
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-02-21 14:40:28 +0100

    Javascript og bad fotoer

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+window.addEventListener("load", AppInit);
+
+function AppInit() {}
+
+function incrementPoints() {
+  console.log("incrementPoints");
+  points++;
+  displayPoints();
+}
