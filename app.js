62a813a2f5a50fc5216b21151e0e0c7877b2937f
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-02-23 13:34:59 +0100

    Startet kald på win/loss funktioner
    Ikke defineret dem - blot lagt dem ind som reminder med kommentarer

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 "use strict";
 window.addEventListener("load", AppInit);
 
-function AppInit() {}
+function AppInit() {
+  console.log("I'm ready! :)");
+}
 
 function incrementPoints() {
   console.log("incrementPoints");
   points++;
   displayPoints();
 }
+
+// Nedenstående er kald på win/lose-funktioner.
+// Selve funktionerne mangler endnu at blive defineret længere nede
+if (lives <= 1) {
+  gameOver();
+} else {
+  levelComplete();
+}
+
+// funktioner til game won/lost
+function gameOver() {}
+function levelComplete() {}
